e54a2813a845bb00afcca3a89ee13ca6e432e248
River Gillis <jrivergillis@gmail.com>
2019-02-18 22:43:22 -0600

    Rename Navigator to AppContainer

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { Provider } from 'react-redux';
 
 import store from './store'; // Import the redux store
 // import PostList from './components/PostList';
-import Navigator from './Navigator';
+import AppContainer from './AppContainer';
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Navigator />
+        <AppContainer />
       </Provider>
     );
   }
